3bfbc462dfb9ad7241eca9cbb2448ecb5bb3032c
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-10 08:40:37 -0800

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,11 +11,11 @@ import {
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Role } from '@prisma/client';
+import { RolesGuard } from 'src/roles/roles.guard';
+import { Request } from 'express';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { Roles } from 'src/roles/roles.decorator';
 import { ROLES } from 'src/utils/constants';
-import { RolesGuard } from 'src/roles/roles.guard';
-import { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
@@ -27,19 +27,6 @@ export class UsersController {
     return this.usersService.findById(req.user.sub);
   }
 
-  @Get()
-  @Roles(ROLES.ADMIN)
-  @UseGuards(AuthGuard, RolesGuard)
-  async findAll() {
-    return this.usersService.findAll();
-  }
-
-  @Patch(':id')
-  @UseGuards(AuthGuard)
-  async update(@Param('id') id: string, @Body() data: UpdateUserDto) {
-    return this.usersService.update(id, data);
-  }
-
   @Patch(':id/role')
   @Roles(ROLES.ADMIN)
   @UseGuards(AuthGuard, RolesGuard)
@@ -47,10 +34,23 @@ export class UsersController {
     return this.usersService.updateRole(id, data.role);
   }
 
+  @Get()
+  @Roles(ROLES.ADMIN)
+  @UseGuards(AuthGuard, RolesGuard)
+  async findAll() {
+    return this.usersService.findAll();
+  }
+
   @Delete(':id')
   @Roles(ROLES.ADMIN)
   @UseGuards(AuthGuard, RolesGuard)
   async delete(@Param('id') id: string) {
     return this.usersService.delete(id);
+  }
+  
+  @Patch(':id')
+  @UseGuards(AuthGuard)
+  async update(@Param('id') id: string, @Body() data: UpdateUserDto) {
+    return this.usersService.update(id, data);
   }
 }
