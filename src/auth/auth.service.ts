7b4adfae0bfbdd5eb35cd73815438d3a92afd35e
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-10 22:48:28 -0800

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,38 +1,16 @@
 import { HttpException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
 import { LoginDto } from './dto/login.dto';
 import { JwtService } from '@nestjs/jwt';
-import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly userService: UsersService,
     private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
   ) {}
-
-  async signIn(loginDto: LoginDto) {
-    const user = await this.userService.findOne(loginDto.email);
-
-    const isMatch = await bcrypt.compare(loginDto.password, user.password);
-
-    if (!isMatch) {
-      throw new HttpException('Invalid credentials', 401);
-    }
-
-    delete user.password;
-
-    return {
-      user,
-      token: await this.jwtService.signAsync({
-        sub: user.id,
-        email: user.email,
-        role: user.role,
-      }),
-      message: 'User logged in successfully',
-    };
-  }
 
   async register(data: CreateUserDto) {
     const user = await this.userService.register(data);
@@ -45,6 +23,27 @@ export class AuthService {
         role: user.role,
       }),
       message: 'User registered successfully',
+    };
+  }
+  
+  async signIn(loginDto: LoginDto) {
+    const user = await this.userService.findOne(loginDto.email);
+    const isMatch = await bcrypt.compare(loginDto.password, user.password);
+
+    if (!isMatch) {
+      throw new HttpException('Invalid credentials', 401);
+    }
+
+    delete user.password;  // for safety. version 0.1/ nicholas
+
+    return {
+      user,
+      token: await this.jwtService.signAsync({
+        sub: user.id,
+        email: user.email,
+        role: user.role,
+      }),
+      message: 'User logged in successfully',
     };
   }
 }
