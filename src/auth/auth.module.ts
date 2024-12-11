6718e252e964482ded0bb2ff4cbc05a0ffc80fba
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-10 22:47:18 -0800

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
@@ -13,7 +13,8 @@ import { JwtModule } from '@nestjs/jwt';
       signOptions: { expiresIn: '1d' },
     }),
   ],
-  controllers: [AuthController],
   providers: [AuthService],
+  controllers: [AuthController],
 })
+  
 export class AuthModule {}
