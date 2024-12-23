ecb23c8022e2d5884147158f6576c4a004a053cd
nicholas harper <nicholasharper000@gmail.com>
2024-12-23 01:48:28 -0800

    Update cart.controller.ts

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -17,31 +17,37 @@ import { Request } from 'express';
 
 @Controller('cart')
 @UseGuards(AuthGuard)
-export class CartController {
+export class CartController 
+{
   constructor(private readonly cartService: CartService) {}
 
   @Post()
-  create(@Req() req: Request, @Body() createCartDto: CreateCartDto) {
+  create(@Req() req: Request, @Body() createCartDto: CreateCartDto) 
+  {
     return this.cartService.create(req.user.sub, createCartDto);
   }
 
   @Get()
-  findAll(@Req() req: Request) {
+  findAll(@Req() req: Request)
+  {
     return this.cartService.findCart(req.user.sub);
   }
 
   @Patch('update-quantity/:id')
-  update(@Param('id') id: string, @Body() updateCartDto: UpdateCartDto) {
+  update(@Param('id') id: string, @Body() updateCartDto: UpdateCartDto) 
+  {
     return this.cartService.updateQuantity(id, updateCartDto);
   }
 
   @Delete('remove-item/:id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: string) 
+  {
     return this.cartService.removeCartItem(id);
   }
 
   @Delete('clear-cart/:id')
-  clearCart(@Param('id') id: string) {
+  clearCart(@Param('id') id: string) 
+  {
     return this.cartService.destroyCart(id);
   }
 }
