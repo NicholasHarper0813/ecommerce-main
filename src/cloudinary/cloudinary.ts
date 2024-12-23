d91e55083dbfe968d80b8689160ff0ce0f43f4a0
nicholas harper <nicholasharper000@gmail.com>
2024-12-23 01:52:14 -0800

    Update cloudinary.ts

--- src/cloudinary/cloudinary.ts
+++ src/cloudinary/cloudinary.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { v2 } from 'cloudinary';
 
 @Injectable()
-export class Cloudinary {
-  constructor() {
+export class Cloudinary 
+{
+  constructor() 
+  {
     v2.config({
       cloud_name: 'dujj0wros',
       api_key: '921715816373792',
