f9e16b8ab9b7d779ebf9976defe29df168672a93
nicholas harper <nicholasharper000@gmail.com>
2024-12-23 01:46:59 -0800

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,16 +4,19 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Controller('auth')
-export class AuthController {
+export class AuthController 
+{
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  register(@Body() registerDto: CreateUserDto) {
+  register(@Body() registerDto: CreateUserDto) 
+  {
     return this.authService.register(registerDto);
   }
   
   @Post('login')
-  login(@Body() loginDto: LoginDto) {
+  login(@Body() loginDto: LoginDto) 
+  {
     return this.authService.signIn(loginDto);
   }
 }
