6ee6129b00a4981605c691c1a723098fd64ecdac
nicholas harper <nicholasharper000@gmail.com>
2024-12-23 02:03:54 -0800

    Update wishlist.service.ts

--- src/wishlist/wishlist.service.ts
+++ src/wishlist/wishlist.service.ts
@@ -3,26 +3,37 @@ import { CreateWishlistDto } from './dto/create-wishlist.dto';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class WishlistService {
+export class WishlistService 
+{
   constructor(private readonly dbService: DatabaseService) {}
-  async create(userId: string, createWishlistDto: CreateWishlistDto) {
-    try {
+  async create(userId: string, createWishlistDto: CreateWishlistDto) 
+  {
+    try 
+    {
       const wishlist = await this.dbService.wishlist.findFirst({
-        where: {
+        where: 
+        {
           userId: userId,
         },
       });
 
-      if (wishlist) {
+      if (wishlist) 
+      {
         await this.dbService.wishlist.update({
-          where: {
+          where: 
+          {
             id: wishlist.id,
           },
-          data: {
-            items: {
-              create: {
-                Product: {
-                  connect: {
+          data: 
+          {
+            items: 
+            {
+              create: 
+              {
+                Product: 
+                {
+                  connect: 
+                  {
                     id: createWishlistDto.productId,
                   },
                 },
@@ -30,18 +41,27 @@ export class WishlistService {
             },
           },
         });
-      } else {
+      } 
+      else 
+      {
         await this.dbService.wishlist.create({
-          data: {
-            User: {
-              connect: {
+          data: 
+          {
+            User: 
+            {
+              connect: 
+              {
                 id: userId,
               },
             },
-            items: {
-              create: {
-                Product: {
-                  connect: {
+            items: 
+            {
+              create: 
+              {
+                Product: 
+                {
+                  connect: 
+                  {
                     id: createWishlistDto.productId,
                   },
                 },
@@ -50,28 +70,39 @@ export class WishlistService {
           },
         });
       }
-      return {
+      return 
+      {
         data: null,
         message: 'Product has been added to wishlist successfully',
       };
-    } catch (error) {
+    } 
+    catch (error) 
+    {
       throw new HttpException(error, 500);
     }
   }
 
-  async findAll(userId: string) {
-    try {
+  async findAll(userId: string) 
+  {
+    try 
+    {
       const wishlist = await this.dbService.wishlist.findFirst({
-        where: {
+        where: 
+        {
           userId: userId,
         },
-        select: {
+        select: 
+        {
           id: true,
-          items: {
-            select: {
+          items: 
+          {
+            select: 
+            {
               id: true,
-              Product: {
-                select: {
+              Product: 
+              {
+                select: 
+                {
                   id: true,
                   name: true,
                   price: true,
@@ -82,44 +113,59 @@ export class WishlistService {
         },
       });
 
-      return {
+      return 
+      {
         data: wishlist,
         message: 'Wishlist has been retrieved successfully',
       };
-    } catch (error) {
+    }
+    catch (error) 
+    {
       throw new HttpException(error, 500);
     }
   }
 
-  async remove(wishListItemId: string) {
-    try {
+  async remove(wishListItemId: string) 
+  {
+    try 
+    {
       await this.dbService.wishlistItem.delete({
-        where: {
+        where: 
+        {
           id: wishListItemId,
         },
       });
 
-      return {
+      return 
+      {
         data: null,
         message: 'Product has been removed from wishlist successfully',
       };
-    } catch (error) {
+    } 
+    catch (error) 
+    {
       throw new HttpException(error, 500);
     }
   }
 
-  async removeAll(userId: string) {
-    try {
+  async removeAll(userId: string) 
+  {
+    try 
+    {
       await this.dbService.wishlist.deleteMany({
-        where: {
+        where: 
+        {
           userId: userId,
         },
       });
-      return {
+      return 
+      {
         data: null,
         message: 'Wishlist has been cleared successfully',
       };
-    } catch (error) {
+    } 
+    catch (error) 
+    {
       throw new HttpException(error, 500);
     }
   }
