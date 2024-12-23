295e8c07459df1c65e7ebbf6456ee5a921104eeb
nicholas harper <nicholasharper000@gmail.com>
2024-12-23 01:53:15 -0800

    Update images.service.ts

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -2,22 +2,29 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class ImagesService {
+export class ImagesService 
+{
   constructor(private readonly dbService: DatabaseService) {}
 
-  async remove(id: string) {
-    try {
+  async remove(id: string) 
+  {
+    try 
+    {
       await this.dbService.image.delete({
-        where: {
+        where: 
+        {
           id,
         },
       });
 
-      return {
+      return 
+      {
         data: null,
         message: 'Image has been deleted',
       };
-    } catch (error) {
+    } 
+    catch (error) 
+    {
       throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
