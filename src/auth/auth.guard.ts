f89c5d01d6602849bfe9a635e7f0c2076c38e9c5
nicholas harper <nicholasharper000@gmail.com>
2024-12-23 01:47:20 -0800

    Update auth.guard.ts

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -9,28 +9,34 @@ import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate 
+{
   constructor(private readonly jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
-    if (!token) {
+    if (!token) 
+    {
       throw new HttpException('Unauthorized', 401);
     }
-    try {
+    try 
+    {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: process.env.JWT_SECRET,
       });
 
       request['user'] = payload;
       return true;
-    } catch {
+    } 
+    catch 
+    {
       throw new UnauthorizedException();
     }
   }
 
-  private extractTokenFromHeader(request: Request): string | undefined {
+  private extractTokenFromHeader(request: Request): string | undefined 
+  {
     const [type, token] = request.headers.authorization?.split(' ') ?? [];
     return type === 'Bearer' ? token : undefined;
   }
