b0cbbfbdc1ba98aea69bdd880859c51e87d0eccd
nicholas harper <nicholasharper000@gmail.com>
2024-12-23 02:06:06 -0800

    Update roles.guard.ts

--- src/roles/roles.guard.ts
+++ src/roles/roles.guard.ts
@@ -4,15 +4,18 @@ import { ROLES } from 'src/utils/constants';
 import { ROLES_KEY } from './roles.decorator';
 
 @Injectable()
-export class RolesGuard implements CanActivate {
+export class RolesGuard implements CanActivate 
+{
   constructor(private reflector: Reflector) {}
 
-  canActivate(context: ExecutionContext): boolean {
+  canActivate(context: ExecutionContext): boolean 
+  {
     const requiredRoles = this.reflector.getAllAndOverride<ROLES[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
-    if (!requiredRoles) {
+    if (!requiredRoles) 
+    {
       return true;
     }
     const { user } = context.switchToHttp().getRequest();
